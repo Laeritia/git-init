2f46e9819554c109f1d97cc7d0a791201e4ece7a
Laeritia <165818681+Laeritia@users.noreply.github.com>
2025-02-09 13:41:42 -0300

    Add files via upload

---
+++ Speeding_Ticket.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string vet[10];
+
+int main(){
+    int n,m,a,b,res=0,i,j;
+    vector<pair<int,int>> v1,v2;
+    freopen("speeding.in", "r", stdin);
+    scanf("%d %d",&n,&m);
+    for(int i=0;i<n;i++){
+        scanf("%d %d",&a,&b);
+        v1.push_back({a,b});
+    }
+    for(int i=0;i<m;i++){
+        scanf("%d %d",&a,&b);
+        v2.push_back({a,b});
+    }
+    i=j=0;
+    while(i<n&&j<m){
+        if(v2[j].second>v1[i].second){
+            if(res<v2[j].second-v1[i].second){
+                res=v2[j].second-v1[i].second;
+            }
+        }
+        if(v1[i].first>v2[j].first){
+            v1[i].first=v1[i].first-v2[j].first;
+            j++;
+            continue;
+        }
+        if(v1[i].first<v2[j].first){
+            v2[j].first=v2[j].first-v1[i].first;
+            i++;
+            continue;
+        }
+        if(v1[i].first==v2[j].first){
+            i++;
+            j++;
+        }
+    }
+    freopen("speeding.out", "w", stdout);
+    cout<<res;
+}
