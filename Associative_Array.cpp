05d5182c8a18d83436bbfc72eca4a802ca133dd0
Laeritia <165818681+Laeritia@users.noreply.github.com>
2025-03-10 19:19:56 -0300

    Add files via upload

--- Associative_Array.cpp
+++ Associative_Array.cpp
@@ -3,24 +3,34 @@
 
 using namespace std;
 
-map<long long,long long> a;
+vector<pair<int,int>> lista;
 
 int main(){
-    int q,i,x;
-    long long y,z;
-    cin>>q;
-    for(i=0;i<q;i++){
-        cin>>x;
-        if(x==1){
-            cin>>y;
-            cout<<a[y];
-            if(i!=q-1){
-                cout<<"\n";
-            }
-        } else{
-            cin>>y>>z;
-            a[y]=z;
+    int n,i,x,y,pesq,pdir,flag=0;
+    cin>>n>>x;
+    for(i=0;i<n;i++){
+        cin>>y;
+        lista.push_back({y,i});
+    }
+    sort(lista.begin(),lista.end());
+    pesq=0;
+    pdir=n-1;
+    while(pdir>pesq){
+        if(lista[pdir].first+lista[pesq].first==x){
+            flag=1;
+            break;
         }
+        if(lista[pdir].first+lista[pesq].first<x){
+            pesq++;
+        }
+        if(lista[pdir].first+lista[pesq].first>x){
+            pdir--;
+        }
+    }
+    if(flag==1){
+        cout<<lista[pesq].second+1<<" "<<lista[pdir].second+1;
+    } else{
+        cout<<"IMPOSSIBLE";
     }
     return 0;
 }
