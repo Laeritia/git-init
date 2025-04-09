61d8dcdfbd1569b195d541c60135aab08dce009e
Laeritia <165818681+Laeritia@users.noreply.github.com>
2025-04-09 03:34:20 -0300

    Add files via upload

---
+++ Maximum_Subarray_Sum.cpp
@@ -0,0 +1,105 @@
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//int lista[100];
+long long soma[200010];
+long long vetor[200010];
+
+int main(){
+    int n,i,index=-1;
+    long long x,sum=0,somaatual=0,somamax=0,soma2=0,flag=0;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>x;
+        sum+=x;
+        soma[i]=sum;
+        vetor[i]=x;
+    }
+    for(i=0;i<n;i++){
+        if(vetor[i]<0){
+            continue;
+        }
+        somaatual=vetor[i];
+        if(i<n-1){
+            while(i<(n-1)&&vetor[i+1]>=0){
+                i++;
+                somaatual+=vetor[i];
+            }
+        }
+        if(flag==0){
+            index=i;
+            soma2=somaatual;
+            somamax=somaatual;
+            flag=1;
+        } else{
+            soma2=max(soma2+soma[i]-soma[index],somaatual);
+            index=i;
+        }
+        if(soma2>somamax){
+            somamax=soma2;
+        }
+    }
+    if(flag==0){
+        sort(vetor, vetor+n);
+        cout<<vetor[n-1];
+    } else{
+        cout<<somamax<<endl;
+    }
+    return 0;
+}
+
+/*freopen("outofplace.in", "r", stdin);
+    freopen("outofplace.out", "w", stdout);
+    int n,i,guardo=-1,flag=0,j,res=0;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>lista[i];
+        if(i!=0){
+            if(lista[i]<lista[i-1]){
+                guardo=i;
+            }
+        }
+    }
+    if(guardo!=0&&guardo!=n-1){
+        if(lista[guardo]<lista[guardo+1]){
+            guardo=guardo-1;
+        }
+    }
+    if(guardo==-1){
+        cout<<0;
+    } else{
+        i=0;
+        while(flag==0){
+            if(lista[i]>=lista[guardo]&&i!=guardo){
+                flag=1;
+                break;
+            }
+            i++;
+        }
+        if(i<guardo){
+            for(j=guardo-1;j>=i;j--){
+                if(j==0){
+                    res++;
+                    break;
+                }
+                while(lista[j-1]==lista[j]){
+                    j--;
+                }
+                res++;
+            }
+        } else{
+            for(j=guardo+1;j<i;j++){
+                if(j==n-1){
+                    res++;
+                    break;
+                }
+                while(lista[j]==lista[j+1]){
+                    j--;
+                }
+                res++;
+            }
+        }
+        cout<<res;
+    }*/
